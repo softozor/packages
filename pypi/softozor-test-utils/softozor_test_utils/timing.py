508669f9a8f6dba566071421961c2047cda66391
Laurent Michel <softozor@gmail.com>
2021-10-27 16:44:33 +0200

    make function fail_after_timeout available

--- pypi/softozor-test-utils/softozor_test_utils/timing.py
+++ pypi/softozor-test-utils/softozor_test_utils/timing.py
@@ -10,3 +10,11 @@ def wait_until(predicate, timeout_in_sec=5, period_in_sec=0.1):
             raise TimeoutError(
                 f'wait_until expired after <{timeout_in_sec}> seconds')
         time.sleep(period_in_sec)
+
+
+def fail_after_timeout(predicate, timeout_in_sec=5, period_in_sec=0.1):
+    try:
+        wait_until(predicate, timeout_in_sec, period_in_sec)
+        return True
+    except TimeoutError:
+        return False
