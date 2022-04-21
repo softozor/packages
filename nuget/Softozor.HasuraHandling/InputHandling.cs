2f3f9ed4f08097f73f5502f57bbce7ba593fbcc1
Laurent Michel <softozor@gmail.com>
2022-04-21 21:52:49 +0200

    add method ExtractJwt

--- nuget/Softozor.HasuraHandling/InputHandling.cs
+++ nuget/Softozor.HasuraHandling/InputHandling.cs
@@ -1,5 +1,7 @@
 ﻿namespace Softozor.HasuraHandling;
 
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Softozor.HasuraHandling.Data;
@@ -7,6 +9,8 @@ using Softozor.HasuraHandling.Properties;
 
 public static class InputHandling
 {
+    private const string DefaultAccessTokenAuthHeaderPrefix = "Bearer";
+
     public static async Task<TInput> ExtractActionRequestPayloadFrom<TInput>(HttpContext http)
         where TInput : class
     {
@@ -16,4 +20,27 @@ public static class InputHandling
 
         return input;
     }
+
+    public static string ExtractJwt(HttpContext http)
+        {
+            var authorization = http.Request.Headers["Authorization"].FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(authorization))
+            {
+                throw new HasuraFunctionException(
+                    "The request contains no authorization header",
+                    StatusCodes.Status400BadRequest);
+            }
+
+            if (!authorization.StartsWith(DefaultAccessTokenAuthHeaderPrefix, StringComparison.OrdinalIgnoreCase))
+            {
+                throw new HasuraFunctionException(
+                    "The authorization header does not contain a Bearer token",
+                    StatusCodes.Status400BadRequest);
+            }
+
+            var token = authorization.Substring(DefaultAccessTokenAuthHeaderPrefix.Length).Trim();
+
+            return token;
+        }
 }
